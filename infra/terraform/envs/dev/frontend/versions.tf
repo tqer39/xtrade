c4a52aeeb349792f60242be91291b1534a8b7198
tqer39 <tqer39@gmail.com>
2025-11-27 08:02:02 +0900

    feat ✨: Terraform initで自動生成されるファイルを追加
    terraform initで自動生成されるファイルを追加する

--- infra/terraform/envs/dev/frontend/versions.tf
+++ infra/terraform/envs/dev/frontend/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.9"
+  required_version = "1.14.0"
   required_providers {
     vercel = {
       source  = "vercel/vercel"
-      version = "~> 1.0"
+      version = "1.3.0"
     }
   }
   backend "s3" {
