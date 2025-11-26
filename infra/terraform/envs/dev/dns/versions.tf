e9064d16538ddd82812f0d2e73f6ff86f7d59e9b
tqer39 <tqer39@gmail.com>
2025-11-27 08:01:40 +0900

    feat ✨: Terraform initで自動生成されるファイルを追加する
    terraform initで自動生成されるファイルを追加する

--- infra/terraform/envs/dev/dns/versions.tf
+++ infra/terraform/envs/dev/dns/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.9"
+  required_version = "1.14.0"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 6.0"
+      version = "6.45.0"
     }
   }
   backend "s3" {
