72429a794a2d4edd5ee2d09465c3a3af08da505e
tqer39 <tqer39@gmail.com>
2025-11-27 08:01:17 +0900

    feat ✨: Neon providerのバージョンを更新
    neon providerのバージョンを0.12.0に更新しました。

--- infra/terraform/envs/dev/database/versions.tf
+++ infra/terraform/envs/dev/database/versions.tf
@@ -1,12 +1,6 @@
 terraform {
   required_version = "1.14.0"
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "6.21.0"
-    }
-  }
-  required_providers {
     neon = {
       source  = "kislerdm/neon"
       version = "0.12.0"
