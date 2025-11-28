275bc95098b0e246d1826dbe4bdc9e6d805f504a
Takeru O'oyama <tqer39@gmail.com>
2025-11-29 05:04:00 +0900

    ✨ コンピュートエンドポイントとロールの作成機能を追加 (#16)
    * feat ✨: .serena/.gitignoreと.project.ymlを作成
    
    * feat ✨:
    
    package.jsonの依存関係を更新しました。
    
    * feat ✨: コンピュートエンドポイントの作成とロールの作成
    
    コンピュートエンドポイントの作成とロールの作成を追加する

--- infra/terraform/modules/neon/main.tf
+++ infra/terraform/modules/neon/main.tf
@@ -19,30 +19,30 @@ resource "neon_project" "this" {
   }
 }
 
-# データベースの作成（プロジェクトのデフォルトブランチに作成）
-resource "neon_database" "this" {
-  project_id = neon_project.this.id
-  branch_id  = neon_project.this.default_branch_id
-  name       = var.database_name
-  owner_name = var.database_owner
-}
-
-# コンピュートエンドポイントの作成
-resource "neon_endpoint" "this" {
-  project_id = neon_project.this.id
-  branch_id  = neon_project.this.default_branch_id
-
-  type                     = "read_write"
-  autoscaling_limit_min_cu = var.compute_min_cu
-  autoscaling_limit_max_cu = var.compute_max_cu
-
-  # サスペンド設定（秒）
-  suspend_timeout_seconds = var.suspend_timeout_seconds
-}
-
 # ロール（ユーザー）の作成
 resource "neon_role" "this" {
   project_id = neon_project.this.id
   branch_id  = neon_project.this.default_branch_id
   name       = var.database_owner
+}
+
+# データベースの作成（プロジェクトのデフォルトブランチに作成）
+resource "neon_database" "this" {
+  project_id = neon_project.this.id
+  branch_id  = neon_project.this.default_branch_id
+  name       = var.database_name
+  owner_name = neon_role.this.name
+
+  depends_on = [neon_role.this]
+}
+
+# デフォルトエンドポイントを参照（プロジェクト作成時に自動で作成される）
+data "neon_branch_endpoints" "this" {
+  project_id = neon_project.this.id
+  branch_id  = neon_project.this.default_branch_id
+}
+
+locals {
+  # デフォルトの read_write エンドポイントを取得
+  default_endpoint = [for ep in data.neon_branch_endpoints.this.endpoints : ep if ep.type == "read_write"][0]
 }
